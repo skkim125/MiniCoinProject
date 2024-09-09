55c171bbc87ae1de3469acb4d558f2cfd48bcd72
skkim125 <kthanks125@gmail.com>
2024-09-09 18:25:42 +0900

    Initial Commit

---
+++ MiniCoinProject/MiniCoinProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCoinProjectApp.swift
+//  MiniCoinProject
+//
+//  Created by 김상규 on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniCoinProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
